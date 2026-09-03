07725be19fdf24cbceb988dab7f11b56002f7f84
Song <vpjokor@gmail.com>
2026-09-03 22:34:07 +0800

    代码优化

--- impl/src/main/java/com/mar2sdk/impl/DebugActivity.kt
+++ impl/src/main/java/com/mar2sdk/impl/DebugActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mar2sdk.core.Core
+import com.mar2sdk.core.policy.UserInfo
 
 class DebugActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +34,8 @@ class DebugActivity : AppCompatActivity() {
 	fun refreshData() {
 		findViewById<TextView>(R.id.app_mod).text = Core.appMod.name
 		findViewById<TextView>(R.id.user_type).text = Core.userType.name
+		findViewById<TextView>(R.id.risk_ip).text = UserInfo.riskIP.name
+		findViewById<TextView>(R.id.risk_package).text = UserInfo.riskPackage.name
 		Toast.makeText(this,"刷新成功", Toast.LENGTH_LONG).show()
 	}
 }

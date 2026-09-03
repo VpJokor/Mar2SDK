8178a40feb03c7e84d41b65b56d8d1746875ae69
Song <vpjokor@gmail.com>
2026-09-03 18:12:52 +0800

    更新Singular配置

--- core/src/main/java/com/mar2sdk/core/firebase/SingularConfig.kt
+++ core/src/main/java/com/mar2sdk/core/firebase/SingularConfig.kt
@@ -1,8 +1,50 @@
 package com.mar2sdk.core.firebase
 
+import com.mar2sdk.core.Core
+import com.mar2sdk.core.R
+import com.mar2sdk.core.util.PreferenceUtil
+import org.json.JSONObject
+
+//Singular的配置
 object SingularConfig {
-	val key = "mar2game_f7b9272a"
-	val secret = "72b3df2ee5d0a64a6c404ce01937c3d6"
+	var key = ""
+	var secret = ""
 	// 是否给Singular上报收入事件
-	val trackRevenue = true
+	var trackRevenue = true
+
+	fun init() {
+		loadConfigFromRaw()
+		loadConfigFromPreference()
+	}
+
+	// 从打包资源读取默认配置。
+	fun loadConfigFromRaw() {
+		val config = Core.app.resources.openRawResource(R.raw.singular_config)
+			.bufferedReader()
+			.use { JSONObject(it.readText()) }
+
+		with(config) {
+			key = getString("key")
+			secret = getString("secret")
+			trackRevenue = getBoolean("trackRevenue")
+		}
+	}
+
+	// 从本地读取配置，未保存的配置项沿用打包资源中的值。
+	fun loadConfigFromPreference() {
+		with(SingularKey) {
+			key = PreferenceUtil.getString(KEY_KEY, key)
+			secret = PreferenceUtil.getString(KEY_SECRET, secret)
+			trackRevenue = PreferenceUtil.getBoolean(KEY_TRACK_REVENUE, trackRevenue)
+		}
+	}
+
+	// 把配置保存到本地 (Preference)。
+	fun saveSingularConfig() {
+		with(SingularKey) {
+			PreferenceUtil.commitString(KEY_KEY, key)
+			PreferenceUtil.commitString(KEY_SECRET, secret)
+			PreferenceUtil.commitBoolean(KEY_TRACK_REVENUE, trackRevenue)
+		}
+	}
 }
---
+++ core/src/main/java/com/mar2sdk/core/firebase/SingularKey.kt
@@ -0,0 +1,8 @@
+package com.mar2sdk.core.firebase
+
+//Singular的配置
+object SingularKey {
+	const val KEY_KEY = "mar2sdk.singular_config.key"
+	const val KEY_SECRET = "mar2sdk.singular_config.secret"
+	const val KEY_TRACK_REVENUE = "mar2sdk.singular_config.trackRevenue"
+}

fd45679b2e9ed5524279ea27c40ed6902d1f0292
Song <vpjokor@gmail.com>
2026-09-03 13:39:26 +0800

    更新配置

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation(libs.androidx.appcompat)
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.material)
+	implementation(libs.kotlinx.coroutines.android)
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
 	androidTestImplementation(libs.androidx.junit)

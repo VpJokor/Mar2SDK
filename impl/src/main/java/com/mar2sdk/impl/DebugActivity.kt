8094951bb361d2cec571a8598ae1f96b779cf2d2
Song <vpjokor@gmail.com>
2026-09-04 12:14:45 +0800

    代码优化

--- impl/src/main/java/com/mar2sdk/impl/DebugActivity.kt
+++ impl/src/main/java/com/mar2sdk/impl/DebugActivity.kt
@@ -9,8 +9,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mar2sdk.core.AppMod
 import com.mar2sdk.core.Core
+import com.mar2sdk.core.policy.TestMod
 import com.mar2sdk.core.policy.UserInfo
+import com.mar2sdk.core.policy.UserType
 
 class DebugActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +34,26 @@ class DebugActivity : AppCompatActivity() {
 		findViewById<View>(R.id.send_test_fcm).setOnClickListener {
 			Toast.makeText(this@DebugActivity, "发送FCM测试信息", Toast.LENGTH_LONG).show()
 		}
+		findViewById<View>(R.id.change_test_mod).setOnClickListener {
+			if (Core.testMod == TestMod.FORCE) {
+				Core.testMod = TestMod.POLICY
+			} else {
+				Core.testMod = TestMod.FORCE
+			}
+			refreshData()
+		}
+		findViewById<View>(R.id.test_user).setOnClickListener {
+			if (Core.appMod == AppMod.TEST && Core.testMod == TestMod.FORCE) {
+				val userIndex = UserType.entries.indexOf(Core.userType)
+				var nextIndex = userIndex + 1
+				if (nextIndex >= UserType.entries.size) nextIndex = 0
+				Core.userType = UserType.entries[nextIndex]
+				refreshData()
+			} else {
+				Toast.makeText(this@DebugActivity, "仅 FORCE 测试模式下可用", Toast.LENGTH_LONG).show()
+			}
+
+		}
 	}
 
 	override fun onResume() {
@@ -44,6 +67,13 @@ class DebugActivity : AppCompatActivity() {
 		findViewById<TextView>(R.id.risk_ip).text = UserInfo.riskIP.name
 		findViewById<TextView>(R.id.risk_package).text = UserInfo.riskPackage.name
 		findViewById<TextView>(R.id.ecpm_type).text = UserInfo.ecpmType.name
+		findViewById<TextView>(R.id.test_mod_name).text = Core.testMod.name
+		if (Core.testMod == TestMod.POLICY) {
+			findViewById<TextView>(R.id.test_user_name).text = "关闭"
+		} else {
+			findViewById<TextView>(R.id.test_user_name).text = Core.userType.name
+		}
+
 		Toast.makeText(this,"刷新成功", Toast.LENGTH_LONG).show()
 	}
 }

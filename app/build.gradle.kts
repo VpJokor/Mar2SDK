7c45cb268e5c5e261425cef218c760fe0f769a57
Song <vpjokor@gmail.com>
2026-09-04 15:44:05 +0800

    代码优化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
 	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(project(":core"))
 	implementation(project(":impl"))
 
 }
--- core/src/main/java/com/mar2sdk/core/AppStatus.kt
+++ core/src/main/java/com/mar2sdk/core/AppStatus.kt
@@ -1,17 +1,23 @@
 package com.mar2sdk.core
 
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
 
 /**
  * APP状态管理类
  */
 object AppStatus {
+	private var registeredApplication: Application? = null
+	private var startedActivityCount = 0
 
 	// 屏幕状态 亮屏/熄屏
 	var isScreenOn = false
 	// 手机状态 锁屏/解锁
 	var isLocked = false
 	// APP是否在前台
-	var isForeground = true
+	@Volatile
+	var isForeground = false
 
 	// 广告状态 (是/否)正在展示全屏广告
 	var isShowingAd = false
@@ -21,5 +27,46 @@ object AppStatus {
 	// 通知状态 上次发送通知的时间
 	var lastNotifyTime = 0L
 
+	@Synchronized
+	fun init() {
+		val application = Core.app
+		if (registeredApplication === application) {
+			return
+		}
 
+		registeredApplication?.unregisterActivityLifecycleCallbacks(activityLifecycleCallbacks)
+		startedActivityCount = 0
+		isForeground = false
+		application.registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
+		registeredApplication = application
+	}
+
+	// 前后台监听监控
+	private val activityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
+		override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
+
+		override fun onActivityStarted(activity: Activity) {
+			if (startedActivityCount == 0) {
+				isForeground = true
+			}
+			startedActivityCount++
+		}
+
+		override fun onActivityResumed(activity: Activity) = Unit
+
+		override fun onActivityPaused(activity: Activity) = Unit
+
+		override fun onActivityStopped(activity: Activity) {
+			if (startedActivityCount > 0) {
+				startedActivityCount--
+			}
+			if (startedActivityCount == 0 && !activity.isChangingConfigurations) {
+				isForeground = false
+			}
+		}
+
+		override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+
+		override fun onActivityDestroyed(activity: Activity) = Unit
+	}
 }

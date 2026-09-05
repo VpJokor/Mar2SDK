b8d34ffefad2557e2ae306d1de4995de66364eee
Song <vpjokor@gmail.com>
2026-09-05 17:37:14 +0800

    代码优化

---
+++ app/src/main/java/com/mar2sdk/ContentScreen.kt
@@ -0,0 +1,21 @@
+package com.mar2sdk
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.mar2sdk.impl.AreaKeys
+import com.mar2sdk.impl.BaseScreen
+
+// debug页面
+@Composable
+fun ContentScreen() {
+	BaseScreen(areaKey = AreaKeys.KEY_TEST) {
+		Box (
+			modifier = Modifier.fillMaxSize()
+		) {
+			Text("内容页")
+		}
+	}
+}

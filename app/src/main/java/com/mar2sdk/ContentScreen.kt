468ad220318e9ceac08f0b22a735f66d4f515bc5
Song <vpjokor@gmail.com>
2026-09-05 18:40:45 +0800

    代码优化

--- app/src/main/java/com/mar2sdk/ContentScreen.kt
+++ app/src/main/java/com/mar2sdk/ContentScreen.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.mar2sdk.impl.BaseScreen
 
 @Composable
 fun ContentScreen1() {
 	BaseScreen {
-		Box(modifier = Modifier.fillMaxSize()) {
+		Box(
+			modifier = Modifier.fillMaxSize(),
+			contentAlignment = Alignment.Center
+		) {
 			Text("内容页面 1")
 		}
 	}
@@ -19,7 +23,10 @@ fun ContentScreen1() {
 @Composable
 fun ContentScreen2() {
 	BaseScreen {
-		Box(modifier = Modifier.fillMaxSize()) {
+		Box(
+			modifier = Modifier.fillMaxSize(),
+			contentAlignment = Alignment.Center
+		) {
 			Text("内容页面 2")
 		}
 	}

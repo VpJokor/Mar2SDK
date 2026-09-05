80e1a284699821601065a838b64a2647e6b2258e
Song <vpjokor@gmail.com>
2026-09-05 16:18:19 +0800

    代码优化

--- impl/build.gradle.kts
+++ impl/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
 	alias(libs.plugins.android.library)
+	alias(libs.plugins.kotlin.compose)
 }
 
 android {
 	namespace = "com.mar2sdk.impl"
+	buildFeatures {
+		compose = true
+	}
 	compileSdk {
 		version = release(37) {
 			minorApiLevel = 0
@@ -24,6 +28,12 @@ android {
 
 dependencies {
 	implementation(libs.androidx.activity.ktx)
+	implementation(platform(libs.androidx.compose.bom))
+	implementation(libs.androidx.activity.compose)
+	implementation(libs.androidx.compose.material3)
+	implementation(libs.androidx.compose.ui)
+	implementation(libs.androidx.compose.ui.tooling.preview)
+	debugImplementation(libs.androidx.compose.ui.tooling)
 	implementation(libs.androidx.appcompat)
 	implementation(libs.androidx.constraintlayout)
 	implementation(libs.androidx.core.ktx)

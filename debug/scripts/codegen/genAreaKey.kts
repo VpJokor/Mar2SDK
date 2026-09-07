43bd85f1cb81c1265671c07f5f5fab0f761881fb
Song <vpjokor@gmail.com>
2026-09-07 10:12:50 +0800

    代码优化

---
+++ debug/scripts/codegen/genAreaKey.kts
@@ -0,0 +1,6 @@
+#!/usr/bin/env kotlin
+
+/**
+ * 根据app和impl模块中内容页已有代码 生成 impl模块中对应的 AreaKeys.kt 文件
+ * app模块为业务代码，页面随时会增加和减少
+ */
--- debug/scripts/codegen/genAreaKey.main.kts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env kotlin
-
-/**
- * 根据已有代码生成 impl模块中 对应的 AreaKeys.kt 文件
- */

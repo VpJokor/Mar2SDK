c3fb1f6b36908f35b1554f67839d023f06c65255
Song <vpjokor@gmail.com>
2026-09-07 15:30:21 +0800

    代码优化

--- core/src/main/java/com/mar2sdk/core/log/LogAdParam.kt
+++ core/src/main/java/com/mar2sdk/core/log/LogAdParam.kt
@@ -53,6 +53,7 @@ internal fun ScreenAdContext.toAdLogParams(
 	put(LogAdParam.ad_areakey, areaKey)
 	put(formatKey, adFormat.name)
 	put(LogAdParam.ad_platform, adPlatform.name)
+	put(LogAdParam.ad_unit_name, adUnitId)
 	put(LogAdParam.trigger, trigger.name)
 	put(LogAdParam.from_route, fromRoute)
 	put(LogAdParam.to_route, toRoute)
@@ -63,6 +64,7 @@ internal fun ScreenAdContext.toSingularAdData(revenue: Double): SingularAdData {
 	return SingularAdData(adPlatform.name, LogAdParam.USD, revenue)
 		.withAdType(adFormat.name)
 		.withAdPlacementName(areaKey)
+		.withAdUnitId(adUnitId)
 		.apply {
 			for ((key, value) in params) {
 				// Keep Singular's platform and mediation platform names consistent.

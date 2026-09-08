cf40100dbdaf88434ee10a202c3fed7580e3efc4
Song <vpjokor@gmail.com>
2026-09-08 13:50:56 +0800

    代码优化

---
+++ core/src/main/java/com/mar2sdk/core/notify/app/NotificationTriggerKey.kt
@@ -0,0 +1,18 @@
+package com.mar2sdk.core.notify.app
+
+object NotificationTriggerKey {
+	const val unlock_home_launcher = "unlock_home_launcher"
+	const val screen_on_a = "screen_on_a"
+	const val screen_on_b = "screen_on_b"
+	const val screen_on_c = "screen_on_c"
+	const val screen_off_locked = "screen_off_locked"
+	const val power_connected = "power_connected"
+	const val return_to_home = "return_to_home"
+	const val ad_click = "ad_click"
+	const val app_exit = "app_exit"
+	const val package_added = "package_added"
+	const val package_removed = "package_removed"
+	const val volume_changed = "volume_changed"
+	const val network_changed = "network_changed"
+	const val boot_restore = "boot_restore"
+}

17cee2a82877df6358a2746d8da2b67e0f59a142
Song <vpjokor@gmail.com>
2026-09-09 14:52:51 +0800

    代码优化

--- debug/src/main/java/com/mar2sdk/debug/AreaKeys.kt
+++ debug/src/main/java/com/mar2sdk/debug/AreaKeys.kt
@@ -1,5 +1,9 @@
 package com.mar2sdk.debug
 
 internal object AreaKeys {
-	const val KEY_TEST = "KEY_TEST"
+	const val KEY_TEST_OPEN = "KEY_TEST_OPEN"
+	const val KEY_TEST_INTER = "KEY_TEST_OPEN"
+	const val KEY_TEST_VIDEO = "KEY_TEST_OPEN"
+	const val KEY_TEST_OPEN_INTER = "KEY_TEST_OPEN_INTER"
+	const val KEY_TEST_INTER_VIDEO = "KEY_TEST_INTER_VIDEO"
 }

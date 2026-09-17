41473c0cf1d2660a27d4b26f1cd0f30ae73e609b
Song <vpjokor@gmail.com>
2026-09-17 10:55:22 +0800

    APP通知文案配置

--- core/src/main/java/com/mar2sdk/core/common/NetUtil.kt
+++ core/src/main/java/com/mar2sdk/core/common/NetUtil.kt
@@ -3,18 +3,27 @@ package com.mar2sdk.core.common
 object NetUtil {
 
 	// TODO: 登录接口
-	fun login() {
+	fun platformLogin() {
 
 	}
 
-	// TODO: 打点接口
-	fun log() {
+	// TODO: 日志上报
+	fun initLog() {
 
 	}
 
-	// TODO: 上报用户信息
-	fun upInfo() {
+	// TODO: 刷新Token
+	fun autoLoginreflushtoken() {
 
 	}
 
+	// TODO: 用户信息上报
+	fun uploadUser() {
+
+	}
+
+	// TODO: 事件上报
+	fun report() {
+
+	}
 }
